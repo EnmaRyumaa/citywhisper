ee09859aaafd67874f69c836475d91190c8c5a24
EnmaRyumaa <fer.augustofs@gmail.com>
2025-07-16 21:26:13 -0300

    Fix StatusController

--- src/main/java/com/citywhisper/controllers/status/StatusController.java
+++ src/main/java/com/citywhisper/controllers/status/StatusController.java
@@ -1,12 +1,14 @@
 package com.citywhisper.controllers.status;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
 
-@RestController("/ping")
+@RestController()
+@RequestMapping(value = "/ping")
 public class StatusController {
 
     @GetMapping
