17d9ebccec88b3d109f0dd1b0b4bf2beb5e44df4
EnmaRyumaa <fer.augustofs@gmail.com>
2025-07-17 09:00:25 -0300

    Add ResponseEntity

--- src/main/java/com/citywhisper/client/RequestClient.java
+++ src/main/java/com/citywhisper/client/RequestClient.java
@@ -4,6 +4,7 @@ import com.citywhisper.client.dto.RequestDTO;
 import com.citywhisper.client.dto.ResponseDTO;
 import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
@@ -14,12 +15,12 @@ public class RequestClient {
     @Autowired
     private WebClient.Builder webClient;
 
-    public Mono<ResponseDTO> makeRequest (String cep) {
-        return webClient.build()
-                .get()
-                .uri("https://brasilapi.com.br/api/cep/v2/" + cep)
-                .retrieve()
-                .bodyToMono(RequestDTO.class)
-                .map(x -> new ResponseDTO(x));
+    public ResponseEntity<Mono<ResponseDTO>> makeRequest (String cep) {
+        return ResponseEntity.ok(webClient.build()
+                    .get()
+                    .uri("https://brasilapi.com.br/api/cep/v2/" + cep)
+                    .retrieve()
+                    .bodyToMono(RequestDTO.class)
+                    .map(x -> new ResponseDTO(x)));
     }
 }
--- src/main/java/com/citywhisper/services/CepService.java
+++ src/main/java/com/citywhisper/services/CepService.java
@@ -6,6 +6,7 @@ import com.citywhisper.client.dto.ResponseDTO;
 import org.apache.coyote.Request;
 import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
@@ -15,8 +16,8 @@ public class CepService {
     @Autowired
     private RequestClient client;
 
-    public Mono<ResponseDTO> getCep(String cep) {
-        Mono<ResponseDTO> response = client.makeRequest(cep);
+    public ResponseEntity<Mono<ResponseDTO>> getCep(String cep) {
+        ResponseEntity<Mono<ResponseDTO>> response = client.makeRequest(cep);
 
         return response;
 
