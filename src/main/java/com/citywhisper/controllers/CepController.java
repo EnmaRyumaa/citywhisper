82a68b280a5fe68e02a7a504bae63185150c344d
EnmaRyumaa <fer.augustofs@gmail.com>
2025-07-16 23:05:53 -0300

    Base for CepController and Service

---
+++ src/main/java/com/citywhisper/controllers/CepController.java
@@ -0,0 +1,22 @@
+package com.citywhisper.controllers;
+
+import com.citywhisper.services.CepService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/cep")
+public class CepController {
+
+    @Autowired
+    private CepService service;
+
+    @GetMapping("/{cep}")
+    public String getCep(@PathVariable String cep) {
+        //TODO trocar retorno para DTO.
+        return service.getCep(cep);
+    }
+}
---
+++ src/main/java/com/citywhisper/services/CepService.java
@@ -0,0 +1,11 @@
+package com.citywhisper.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CepService {
+
+    public String getCep(String cep) {
+        return cep;
+    }
+}
