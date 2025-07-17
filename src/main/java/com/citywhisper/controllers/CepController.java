4953758a205f545d9f1e1495f58408ded9b1ea52
EnmaRyumaa <fer.augustofs@gmail.com>
2025-07-17 09:10:03 -0300

    Change properties

--- src/main/java/com/citywhisper/controllers/CepController.java
+++ src/main/java/com/citywhisper/controllers/CepController.java
@@ -3,6 +3,7 @@ package com.citywhisper.controllers;
 import com.citywhisper.client.dto.ResponseDTO;
 import com.citywhisper.services.CepService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,8 +18,8 @@ public class CepController {
     private CepService service;
 
     @GetMapping("/{cep}")
-    public Mono<ResponseDTO> getCep(@PathVariable String cep) {
+    public ResponseEntity<Mono<ResponseDTO>> getCep(@PathVariable String cep) {
         //TODO trocar retorno para DTO.
-        return service.getCep(cep);
+        return ResponseEntity.ok(service.getCep(cep)).getBody();
     }
 }
