90b94cb06eb2d278505d2fb10e980234a7d0b9da
EnmaRyumaa <fer.augustofs@gmail.com>
2025-07-25 15:30:49 -0300

    add v1 in route

--- src/main/java/com/citywhisper/controllers/status/StatusController.java
+++ src/main/java/com/citywhisper/controllers/status/StatusController.java
@@ -8,7 +8,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestController()
-@RequestMapping(value = "/ping")
+@RequestMapping(value = "/v1/ping")
 public class StatusController {
 
     @GetMapping
