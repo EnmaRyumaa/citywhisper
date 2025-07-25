fa52f81d967689e8dc46928393472a88b39a4484
EnmaRyumaa <fer.augustofs@gmail.com>
2025-07-25 16:33:08 -0300

    Add dto took to capture request

--- src/main/java/com/citywhisper/controllers/CepController.java
+++ src/main/java/com/citywhisper/controllers/CepController.java
@@ -1,13 +1,11 @@
 package com.citywhisper.controllers;
 
+import com.citywhisper.dto.CepDTO;
 import com.citywhisper.dto.ResponseDTO;
 import com.citywhisper.services.CepService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/v1/cep")
@@ -16,12 +14,11 @@ public class CepController {
     @Autowired
     private CepService service;
 
-    @GetMapping("/{cep}")
-    public ResponseEntity<ResponseDTO> getCep(@PathVariable String cep) {
+    @GetMapping()
+    public ResponseEntity<ResponseDTO> getCep(@RequestBody CepDTO cep) {
         //TODO trocar retorno para DTO.
 
-        ResponseDTO dto = new ResponseDTO();
-        dto = service.getCep(cep);
+        ResponseDTO dto = service.getCep(cep.getCep());
 
         return ResponseEntity.ok(dto);
     }
--- src/main/java/com/citywhisper/dto/CepDTO.java
+++ src/main/java/com/citywhisper/dto/CepDTO.java
@@ -1,4 +1,16 @@
 package com.citywhisper.dto;
 
 public class CepDTO {
+
+    public CepDTO() {}
+
+    private String cep;
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
 }
